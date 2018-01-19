6f7835f66319d43a093e739e57fdf912f850360d
keith_L <keithluong42@gmail.com>
2018-01-19 01:39:30 -0800

    Homescreen with inputs

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,12 +13,20 @@ import{
 class Home extends React.Component{
     render(){
         return(
-            <View style={{flex: 1, alignContent:'center', alignItems:'center'}}>
-                {/*<Text style={styles.title}>
-                    Homepage
-                </Text>*/}
-                <Image style={{height:60, width:200}}
-                       source={{uri:'http://chilios.com/wp-content/uploads/2017/10/chilios_logo7-1.png'}}/>
+            <View>
+                <View style={{flex: 1, alignContent:'center', alignItems:'center'}}>
+                    <Text style={styles.title}>
+                        Homepage
+                    </Text>
+                    <View style={{flex:1,top:130, alignContent:'center', alignItems:'center'}}>
+                        <Image style={{height:60, width:200, marginBottom:50}}
+                               source={{uri:'http://chilios.com/wp-content/uploads/2017/10/chilios_logo7-1.png'}}/>
+                        <Text style={styles.welcome}>Welcome!</Text>
+                        <Text style={styles.intro}>Please sign in to save your receipts and invoices.</Text>
+                        <TextInput style={styles.inputField} placeholder={'Email'}/>
+                        <TextInput style={styles.inputField} placeholder={'Password'}/>
+                    </View>
+                </View>
             </View>
         );
     }
@@ -28,6 +36,22 @@ var styles = StyleSheet.create({
     title:{
         fontSize:24,
         textAlign: 'center',
+    },
+    welcome:{
+        fontFamily:'cardo',
+        fontSize:24,
+        color:'#4aabf4'
+    },
+    intro:{
+
+    },
+    inputField:{
+        margin:3,
+        width:220,
+        height:40,
+        fontSize:20,
+        borderWidth:2,
+        borderColor:'grey'
     },
     login:{
         height:40,
